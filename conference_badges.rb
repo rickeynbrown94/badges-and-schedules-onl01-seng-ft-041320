5383aed6edfb9ff56cae851cef0e3deb15987da2
Rickey Brown <rickeynbrown94@gmail.com>
2020-04-21 00:22:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,12 @@ def badge_maker(name)
  "Hello, my name is #{name}."
 end
 
+
 def batch_badge_creator(attendees)
   badges = []
   attendees.each { |name| badges << "Hello, my name is #{name}." }
   badges
 end
+
+
+def assign_rooms(attendees)
